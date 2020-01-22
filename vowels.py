c279bd318bc6a82f24b99f3012318e46edfc742d
maaz619 <Ahmedmaaz919@gmail.com>
2020-01-22 22:31:44 +0530

    Updated

--- vowels.py
+++ vowels.py
@@ -6,6 +6,8 @@ vowels=['a','A','e','E','i','I','O','o','U','u']
 for i in name:
     if i in vowels and i.lower() not in total_vowels:
         total_vowels.append(i.lower())
+    else:
+        print('Your name does not contain any vowels.Really?')
 for i in total_vowels:
     count+=1
 if len(total_vowels)==len(main_vowels):
