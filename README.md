e09c92d6c0e6ce3111b871c934fb7881e592fac8
Maaz Ahmed <maazahmedlfc@gmail.com>
2020-01-22 21:47:42 +0530

    proper

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a program, which will tell you the numbers of VOWELS your name contains and what are they.
+
+
---
+++ vowels.py
@@ -0,0 +1,17 @@
+name=list(input('Enter Your name:'))
+count=0
+total_vowels=[]
+main_vowels=['a','e','i','o','u']
+vowels=['a','A','e','E','i','I','O','o','U','u']
+for i in name:
+    if i in vowels:
+        if i.lower() not in total_vowels:
+            total_vowels.append(i.lower())
+for i in total_vowels:
+    count+=1
+if len(total_vowels)==len(main_vowels):
+    print(f'Your name containts all types of vowels {main_vowels}')
+else:
+    total_vowels.sort()
+    print(f'Your name contains {count} types of vowels and here are they {total_vowels}')
+
