f5fac9922d597442619064ad017a44d8e26a3485
maaz619 <Ahmedmaaz919@gmail.com>
2020-01-22 22:20:15 +0530

    Updated

--- vowels.py
+++ vowels.py
@@ -4,9 +4,8 @@ total_vowels=[]
 main_vowels=['a','e','i','o','u']
 vowels=['a','A','e','E','i','I','O','o','U','u']
 for i in name:
-    if i in vowels:
-        if i.lower() not in total_vowels:
-            total_vowels.append(i.lower())
+    if i in vowels and i.lower() not in total_vowels:
+        total_vowels.append(i.lower())
 for i in total_vowels:
     count+=1
 if len(total_vowels)==len(main_vowels):
@@ -14,4 +13,3 @@ if len(total_vowels)==len(main_vowels):
 else:
     total_vowels.sort()
     print(f'Your name contains {count} types of vowels and here are they {total_vowels}')
-
